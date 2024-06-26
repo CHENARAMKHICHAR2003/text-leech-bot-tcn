4d9303122b3e161ed91f87e79ca30c289cb8afcf
sw1gg3r <172894794+sw1gg3r@users.noreply.github.com>
2024-06-26 07:52:28 +0530

    Update core.py

--- modules/core.py
+++ modules/core.py
@@ -181,8 +181,8 @@ async def download_video(url,cmd, name):
 
 
 async def send_doc(bot: Client, m: Message,cc,ka,cc1,prog,count,name):
-    reply = await m.reply_text(f"𝗨𝗽𝗹𝗼𝗮𝗱𝗶𝗻𝗴....\n\n𝐏𝐝𝐟 𝐍𝐚𝐦𝐞 : `{name}`")
-    time.sleep(1)
+    #reply = await m.reply_text(f"𝗨𝗽𝗹𝗼𝗮𝗱𝗶𝗻𝗴....\n\n𝐏𝐝𝐟 𝐍𝐚𝐦𝐞 : `{name}`")
+    #time.sleep(1)
     start_time = time.time()
     await m.reply_document(ka,caption=cc1)
     count+=1
@@ -194,8 +194,8 @@ async def send_doc(bot: Client, m: Message,cc,ka,cc1,prog,count,name):
 
 async def send_vid(bot: Client, m: Message,cc,filename,thumb,name,prog):
     subprocess.run(f'ffmpeg -i "{filename}" -ss 00:01:00 -vframes 1 "{filename}.jpg"', shell=True)
-    await prog.delete (True)
-    reply = await m.reply_text(f"**𝗨𝗽𝗹𝗼𝗮𝗱𝗶𝗻𝗴....**\n\n𝐕𝐢𝐝𝐞𝐨 𝐍𝐚𝐦𝐞 : `{name}`")
+    #await prog.delete (True)
+    #reply = await m.reply_text(f"**𝗨𝗽𝗹𝗼𝗮𝗱𝗶𝗻𝗴....**\n\n𝐕𝐢𝐝𝐞𝐨 𝐍𝐚𝐦𝐞 : `{name}`")
     try:
         if thumb == "no":
             thumbnail = f"{filename}.jpg"
